d3354534126f03911544a3efc55a060a6c36e28a
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-10 01:42:02 +0300

    fifty per task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,11 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const inputDate = document.querySelector('#datetime-picker');
+// const inputDate = document.querySelector('#datetime-picker');
+// const inputDate = document.querySelector('input[type="text"]');
+const startBtn = document.querySelector('button[data-start]');
+
+startBtn.setAttribute('disabled', '');
 
 const options = {
   enableTime: true,
@@ -9,8 +13,48 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (options.defaultDate > selectedDates[0]) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startBtn.removeAttribute('disabled');
+      let timerID = null;
+      startBtn.addEventListener('click', () => {
+        startBtn.setAttribute('disabled', '');
+        timerID = setInterval(() => {
+          const resultDate = convertMs(selectedDates[0] - Date.now());
+          console.log(resultDate);
+          if (
+            resultDate.days === 0 &&
+            resultDate.hours === 0 &&
+            resultDate.minutes === 0 &&
+            resultDate.seconds === 0
+          ) {
+            clearInterval(timerID);
+            console.log('clear');
+          }
+        }, 1000);
+      });
+    }
   },
 };
-inputDate.addEventListener('click', flatpickr);
-flatpickr(element, {});
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}

cda39996ad5dcb5bd1b17e71e998c416c9f61aef
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-09 14:55:44 +0300

    HW-9 task- 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const backgroundBody = document.querySelector('body');
+let timerChangColor = null;
 
+startBtn.addEventListener('click', () => {
+  timerChangColor = setInterval(() => {
+    backgroundBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  backgroundBody.style.backgroundColor = getRandomHexColor();
+  startBtn.setAttribute('disabled', '');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerChangColor);
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}

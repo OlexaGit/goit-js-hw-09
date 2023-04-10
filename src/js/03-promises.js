a711556dd38004bd846a24eb70200be423162d81
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-10 23:04:37 +0300

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,53 +5,49 @@ form.addEventListener('submit', onClickSubmit);
 
 function onClickSubmit(e) {
   e.preventDefault();
-  console.log('hi');
+  // console.log('hi');
   const formElements = e.currentTarget.elements;
-  const delay = formElements.delay.value;
-  const step = formElements.step.value;
-  const amount = formElements.amount.value;
+  const delayEl = Number(formElements.delay.value);
+  const step = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
   // console.log('delay:', delay);
   // console.log('step:', step);
-  // console.log('amount:', amount);
-  // createPromise(amount, delay);
-  // // for (let i = 1; i <= amountEl; i += 1) {
-
-  createPromise(amount, 3000)
-    .then(({ position, delay }) => {
-      console.log('delayCR:', delay);
-      console.log('position:', position);
-      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-  // }
+  // console.log(amount + 1);
+  let delay = delayEl;
+  console.log('delay:', delay);
+  for (let i = 1; i < amount + 1; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        // console.log('delayCR:', delay);
+        // console.log('position:', position);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay = delay + step;
+    // console.log('delayEl + step:', delay);
+    // console.log('i:', i);
+  }
 }
 
 function createPromise(position, delay) {
-  console.log('delayCR:', delay);
-  console.log('position:', position);
+  // console.log('delayCR:', delay);
+  // console.log('position:', position);
   return new Promise((resolve, reject) => {
-    // const shouldResolve = Math.random() > 0.3;
-    // setTimeout(() => {
-    //   if (shouldResolve) {
-    //     resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    //     // Fulfill
-    //   } else {
-    //     reject(`❌ Rejected promise ${position} in ${delay}ms`);
-    //     // Reject
-    //   }
-    // }, delay);
-
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      // console.log('shouldResolveTrue:', shouldResolve);
-      // Fulfill
-      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } else {
-      // console.log('shouldResolveFalse:', shouldResolve);
-      // Reject
-      reject(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // Fulfill
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        // Reject
+      }
+    }, delay);
   });
 }
